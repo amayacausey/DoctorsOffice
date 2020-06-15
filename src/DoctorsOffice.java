b129b5c4bba6274e2c1b9f12464b47f8de0ae5cb
amayacausey <amayacausey@gmail.com>
2020-06-15 14:46:56 -0700

    first commit

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,22 @@
+public class DoctorsOffice {
+
+    public static void main(String[] args) {
+
+        String patientName="Stacey Manson";
+        boolean hasInsurance=false;
+        double temp=102.2;
+        int numVisits=5;
+        double copay=100.00;
+        String nextApptDate="06/15/2020";
+        String contactNumb="(555)555-5555";
+
+        System.out.println("Patient Details");
+        System.out.println("Name: " +patientName);
+        System.out.println("Patient has insurance: " +hasInsurance);
+        System.out.println("Body Temperature: " +temp);
+        System.out.println("Number of Visits: " +numVisits);
+        System.out.println("Patient's Copay Amount: " +copay);
+        System.out.println("Next Appointment Date: " +nextApptDate);
+        System.out.println("Contact Number: " +contactNumb); }
+
+    }
